8e382dec17b4fa094cf2651800d9d21d47e70c50
Alecia <aleciabenjamin@gmail.com>
2019-02-17 20:21:04 +0100

    add feature tests for publisher can create articles

--- features/step_definitions/assertions.rb
+++ features/step_definitions/assertions.rb
@@ -5,3 +5,7 @@ end
 Then("I should see {string}") do |expected_content|
     expect(page).to have_content expected_content
 end
+
+Then("I should be on {string} page") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,5 +1,22 @@
 When("I visit the site") do
     visit '/'
 end
+
+Given("I visit the landing page") do
+    visit root_path
+  end
+  
+  When("I click {string} link") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click {string} button") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
   
   
